ab93488caa659fe9b36080031cd319bca9d4fb58
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2022-07-07 09:41:06 +0100

    Fix next errors

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,6 +1,6 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
-import Link from 'next/link'
+import Link from 'next/link';
 // import {useRouter} from 'next/router'
 // import logo from './public/logo.png'
 import { useState } from 'react';
