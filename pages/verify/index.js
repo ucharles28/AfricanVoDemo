0a8d3658c4d15dad2280afbb21a11af0932b9b4a
Ikechukwu Uzoma <uzomacharles28@outlook.com>
2022-07-22 23:51:33 +0100

    Verify user page bug fixes

--- pages/verify/index.js
+++ pages/verify/index.js
@@ -1,9 +1,9 @@
 import { useRouter } from 'next/router'
-import { get } from '../../helpers/Api'
+import { get } from '../../helpers/ApiRequest'
 import Verify from '../../components/Verify'
 
 
-export default Verify = () => {
+export default function VerifyPage() {
     const { query } = useRouter();
     const token = parseInt(query.token);
 
